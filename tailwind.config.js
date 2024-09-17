db22d47f2e839b160fd26c77073137e3ab4b611a
Andrew Hahn <andrew@circa42.com>
2024-09-17 11:47:22 -0700

    240917

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     './src/**/*.js',
   ],
       theme: {
+        screens: {
+          'xs': '320px',
+          'sm': '640px',    
+          'md': '768px',    
+          'lg': '1024px',    
+          'xl': '1280px',    
+          '2xl': '1536px',        },
         extend: {
           fontFamily: {
             sans: ['Inter', 'sans-serif'],
