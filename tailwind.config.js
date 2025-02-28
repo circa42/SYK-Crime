3acff5503d8a90a0b34f1f836593cce57ab37b76
Andrew Hahn <andrew@circa42.com>
2025-02-27 21:26:27 -0800

    Explicit Categories

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,8 @@ module.exports = {
           'md': '768px',    
           'lg': '1024px',    
           'xl': '1280px',    
-          '2xl': '1536px',        },
+          '2xl': '1536px',        
+        },
         extend: {
           fontFamily: {
             sans: ['Inter', 'sans-serif'],
